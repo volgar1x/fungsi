0fb3c6db5bd8407793fe52571a431ca149e10081
Blackrush <blackrushx@gmail.com>
2014-06-01 13:58:26 +0200

    adds default methods

--- core/src/main/java/org/fungsi/concurrent/Promise.java
+++ core/src/main/java/org/fungsi/concurrent/Promise.java
@@ -4,4 +4,12 @@ import org.fungsi.Either;
 
 public interface Promise<T> extends Future<T> {
 	void set(Either<T, Throwable> e);
+
+    default void complete(T o) {
+        set(Either.success(o));
+    }
+
+    default void fail(Throwable cause) {
+        set(Either.failure(cause));
+    }
 }
