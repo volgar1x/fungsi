93152eaf103591f3afe74c95cca6851fd9d9f198
Blackrush <blackrushx@gmail.com>
2014-06-08 19:55:58 +0200

    remove meaningless methods

--- core/src/main/java/org/fungsi/concurrent/Timers.java
+++ core/src/main/java/org/fungsi/concurrent/Timers.java
@@ -3,30 +3,28 @@ package org.fungsi.concurrent;
 import org.fungsi.function.UnsafeSupplier;
 
 import java.time.Duration;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 
 public final class Timers {
 	private Timers() {}
 
 	public static Timer wrap(ScheduledExecutorService executor) {
-		return new Timer() {
-			@Override
-			public <T> Future<T> schedule(Duration interval, UnsafeSupplier<T> fn) {
-				Promise<T> p = Promises.create();
-				executor.schedule(() -> p.set(fn.safelyGet()), interval.toNanos(), TimeUnit.NANOSECONDS);
-				return p;
-			}
-		};
+		return new TimerImpl(executor);
 	}
 
-	public static Timer newTimer() {
-		return wrap(Executors.newSingleThreadScheduledExecutor());
-	}
+    static class TimerImpl implements Timer {
+        private final ScheduledExecutorService executor;
 
-	public static Timer newTimer(ThreadFactory threads) {
-		return wrap(Executors.newSingleThreadScheduledExecutor(threads));
-	}
+        TimerImpl(ScheduledExecutorService executor) {
+            this.executor = executor;
+        }
+
+        @Override
+        public <T> Future<T> schedule(Duration interval, UnsafeSupplier<T> fn) {
+            Promise<T> p = Promises.create();
+            executor.schedule(() -> p.set(fn.safelyGet()), interval.toNanos(), TimeUnit.NANOSECONDS);
+            return p;
+        }
+    }
 }
