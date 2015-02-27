77d88b339408965c388be3f0a25379b7e69a07d2
Blackrush <blackrushx@gmail.com>
2015-02-27 18:57:46 +0100

    Fix dead locks

--- core/src/main/java/org/fungsi/concurrent/TransformedFuture.java
+++ core/src/main/java/org/fungsi/concurrent/TransformedFuture.java
@@ -1,70 +1,71 @@
 package org.fungsi.concurrent;
 
 import org.fungsi.Either;
-import org.fungsi.Throwables;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Instant;
 import java.util.Optional;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 final class TransformedFuture<T, R> implements Future<R> {
-    private volatile Future<R> result;
-    private final CountDownLatch resultSyn = new CountDownLatch(1);
+    final Future<T> parent;
+    final Function<Either<T, Throwable>, Future<R>> transformer;
 
-    private List<Consumer<Either<R, Throwable>>> responders = new ArrayList<>();
+    volatile Future<R> result;
 
-    TransformedFuture(Future<T> parent, Function<Either<T, Throwable>, Future<R>> fn) {
-        parent.respond(e -> {
-            result = fn.apply(e);
-            resultSyn.countDown();
+    TransformedFuture(Future<T> parent, Function<Either<T, Throwable>, Future<R>> transformer) {
+        this.parent = parent;
+        this.transformer = transformer;
 
-            responders.forEach(result::respond);
-            responders = null;
-        });
+        this.parent.respond(this::getResult);
+    }
+
+    synchronized Future<R> getResult(Either<T, Throwable> e) {
+        Future<R> r = this.result;
+        if (r != null) {
+            return r;
+        }
+        return this.result = transformer.apply(e);
     }
 
     @Override
     public Optional<Either<R, Throwable>> poll() {
+        return parent.poll().map(this::getResult)
+                .flatMap(Future::poll);
+    }
+
+    @Override
+    public synchronized R get() {
         if (result == null) {
-            return Optional.empty();
+            try {
+                wait();
+            } catch (InterruptedException ignored) { }
         }
-        return result.poll();
+        return result.get();
     }
 
     @Override
-    public R get() {
-        try {
-            resultSyn.await();
-            return result.get();
-        } catch (InterruptedException e) {
-            throw Throwables.propagate(e);
-        }
-    }
+    public synchronized R get(Duration timeout) {
+        if (result == null) {
+            Instant start = Instant.now();
+            try {
+                wait(timeout.toMillis());
+            } catch (InterruptedException ignored) { }
+            Instant end = Instant.now();
 
-    @Override
-    public R get(Duration timeout) {
-        try {
-            if (!resultSyn.await(timeout.toNanos(), TimeUnit.NANOSECONDS)) {
-                throw new TimeoutException(timeout.toString());
-            }
+            return result.get(timeout.minus(Duration.between(start, end)));
+        } else {
             return result.get(timeout);
-        } catch (InterruptedException e) {
-            throw Throwables.propagate(e);
         }
     }
 
     @Override
     public void respond(Consumer<Either<R, Throwable>> fn) {
-        if (result != null) {
-            result.respond(fn);
-        } else {
-            responders.add(fn);
-        }
+        parent.respond(e -> {
+            Future<R> f = getResult(e);
+            f.respond(fn);
+        });
     }
 
     @Override
